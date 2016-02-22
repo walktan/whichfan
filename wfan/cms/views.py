17c340c372c0dc076428c43efcae8a4c9611275a
walktan <yb1.2st.50@gmail.com>
2016-02-23 05:16:47 +0900

    20160223 insert data

--- wfan/cms/views.py
+++ wfan/cms/views.py
@@ -1,20 +1,11 @@
 from django.shortcuts import render
-
-# Create your views here.
 from django.http import HttpResponseRedirect, HttpResponse
 from cms.models import twitDra,twitSwa,twitGia,twitTig,twitCar,twitBay
-from cms.models import twiDra,twiSwa,twiGia,twiTig,twiCar,twiBay
-from requests_oauthlib import OAuth1Session
 import json,string
 from datetime import datetime,timedelta
-from django.db.models import Max,Min
 from django.shortcuts import render
 from django.http import HttpResponse,Http404
-from django.core import serializers
-from django.shortcuts import render_to_response
-from django.template import RequestContext
 from collections import OrderedDict
-import datetime
 
 ### Constants
 oath_key_dict = {
@@ -25,7 +16,6 @@ oath_key_dict = {
 }
 
 def index(request):
-    newinsert()
     return render(request, 'cms/index.html',)
 
 def get_json(req):
@@ -52,8 +42,8 @@ def for_hourly(req):    # AJAXに答える関数
             to_time = datetime.strptime(get_to_time, '%Y/%m/%d %H:%M').replace(minute=0, second=0, microsecond=0)
         except (KeyError,ValueError):
             print ("KeyError?")
-            from_time = datetime.now().replace(minute=0, second=0, microsecond=0) - timedelta(hours=240)
-            to_time = datetime.now().replace(minute=0, second=0, microsecond=0) - timedelta(hours=231)
+            from_time = datetime.now().replace(minute=0, second=0, microsecond=0) - timedelta(hours=11)
+            to_time = datetime.now().replace(minute=0, second=0, microsecond=0)
 
         print(from_time)
         print(to_time)
@@ -94,8 +84,8 @@ def for_dayly(req):    # AJAXに答える関数
             to_time = datetime.strptime(get_to_time, '%Y/%m/%d %H:%M').replace(hour=0, minute=0, second=0, microsecond=0)
         except (KeyError,ValueError):
             print ("KeyError?")
-            from_time = datetime.now().replace(hour=0, minute=0, second=0, microsecond=0) - timedelta(days=14)
-            to_time = datetime.now().replace(hour=0, minute=0, second=0, microsecond=0) - timedelta(days=8)
+            from_time = datetime.now().replace(hour=0, minute=0, second=0, microsecond=0) - timedelta(days=11)
+            to_time = datetime.now().replace(hour=0, minute=0, second=0, microsecond=0)
 
         print(from_time)
         print(to_time)
@@ -123,102 +113,3 @@ def for_dayly(req):    # AJAXに答える関数
 
     else:
         raise Http404
-
-def insert():
-    id_Dra = twitDra.objects.aggregate(max=Min('twit_id'))
-    tweetsDra = tweet_search('中日ドラゴンズ -rt -bot',id_Dra["max"], oath_key_dict)
-    id_Swa = twitSwa.objects.aggregate(max=Min('twit_id'))
-    tweetsSwa = tweet_search('ヤクルトスワローズ -rt -bot',id_Swa["max"], oath_key_dict)
-    id_Gia = twitGia.objects.aggregate(max=Min('twit_id'))
-    tweetsGia = tweet_search('読売ジャイアンツ -rt -bot',id_Gia["max"], oath_key_dict)
-    id_Tig = twitTig.objects.aggregate(max=Min('twit_id'))
-    tweetsTig = tweet_search('阪神タイガース -rt -bot',id_Tig["max"], oath_key_dict)
-    id_Car = twitCar.objects.aggregate(max=Min('twit_id'))
-    tweetsCar = tweet_search('広島カープ -rt -bot',id_Car["max"], oath_key_dict)
-    id_Bay = twitBay.objects.aggregate(max=Min('twit_id'))
-    tweetsBay = tweet_search('横浜ベイスターズ -rt -bot',id_Bay["max"], oath_key_dict)
-    for tweet in tweetsDra["statuses"]:
-        twit_at = change_ja_time(tweet[u'created_at'])
-        p = twitDra(twit_id=tweet[u'id_str'],twit=tweet[u'text'],twit_at=twit_at).save()
-    for tweet in tweetsSwa["statuses"]:
-        twit_at = change_ja_time(tweet[u'created_at'])
-        p = twitSwa(twit_id=tweet[u'id_str'],twit=tweet[u'text'],twit_at=twit_at).save()
-    for tweet in tweetsGia["statuses"]:
-        twit_at = change_ja_time(tweet[u'created_at'])
-        p = twitGia(twit_id=tweet[u'id_str'],twit=tweet[u'text'],twit_at=twit_at).save()
-    for tweet in tweetsTig["statuses"]:
-        twit_at = change_ja_time(tweet[u'created_at'])
-        p = twitTig(twit_id=tweet[u'id_str'],twit=tweet[u'text'],twit_at=twit_at).save()
-    for tweet in tweetsCar["statuses"]:
-        twit_at = change_ja_time(tweet[u'created_at'])
-        p = twitCar(twit_id=tweet[u'id_str'],twit=tweet[u'text'],twit_at=twit_at).save()
-    for tweet in tweetsBay["statuses"]:
-        twit_at = change_ja_time(tweet[u'created_at'])
-        p = twitBay(twit_id=tweet[u'id_str'],twit=tweet[u'text'],twit_at=twit_at).save()
-    return
-
-def newinsert():
-    id_Dra = twiDra.objects.aggregate(max=Min('twit_id'))
-    tweetsDra = tweet_search('中日ドラゴンズ -rt -bot since:2016-02-12',id_Dra["max"], oath_key_dict)
-    id_Swa = twiSwa.objects.aggregate(max=Min('twit_id'))
-    tweetsSwa = tweet_search('ヤクルトスワローズ -rt -bot since:2016-02-12',id_Swa["max"], oath_key_dict)
-    id_Gia = twiGia.objects.aggregate(max=Min('twit_id'))
-    tweetsGia = tweet_search('読売ジャイアンツ -rt -bot since:2016-02-12',id_Gia["max"], oath_key_dict)
-    id_Tig = twiTig.objects.aggregate(max=Min('twit_id'))
-    tweetsTig = tweet_search('阪神タイガース -rt -bot since:2016-02-12',id_Tig["max"], oath_key_dict)
-    id_Car = twiCar.objects.aggregate(max=Min('twit_id'))
-    tweetsCar = tweet_search('広島カープ -rt -bot since:2016-02-12',id_Car["max"], oath_key_dict)
-    id_Bay = twiBay.objects.aggregate(max=Min('twit_id'))
-    tweetsBay = tweet_search('横浜ベイスターズ -rt -bot since:2016-02-12',id_Bay["max"], oath_key_dict)
-    for tweet in tweetsDra["statuses"]:
-        twit_at = change_ja_time(tweet[u'created_at'])
-        p = twiDra(twit_id=tweet[u'id_str'],twit=tweet[u'text'],twit_at=twit_at).save()
-    for tweet in tweetsSwa["statuses"]:
-        twit_at = change_ja_time(tweet[u'created_at'])
-        p = twiSwa(twit_id=tweet[u'id_str'],twit=tweet[u'text'],twit_at=twit_at).save()
-    for tweet in tweetsGia["statuses"]:
-        twit_at = change_ja_time(tweet[u'created_at'])
-        p = twiGia(twit_id=tweet[u'id_str'],twit=tweet[u'text'],twit_at=twit_at).save()
-    for tweet in tweetsTig["statuses"]:
-        twit_at = change_ja_time(tweet[u'created_at'])
-        p = twiTig(twit_id=tweet[u'id_str'],twit=tweet[u'text'],twit_at=twit_at).save()
-    for tweet in tweetsCar["statuses"]:
-        twit_at = change_ja_time(tweet[u'created_at'])
-        p = twiCar(twit_id=tweet[u'id_str'],twit=tweet[u'text'],twit_at=twit_at).save()
-    for tweet in tweetsBay["statuses"]:
-        twit_at = change_ja_time(tweet[u'created_at'])
-        p = twiBay(twit_id=tweet[u'id_str'],twit=tweet[u'text'],twit_at=twit_at).save()
-    return
-
-
-
-def create_oath_session(oath_key_dict):
-    oath = OAuth1Session(
-    oath_key_dict["consumer_key"],
-    oath_key_dict["consumer_secret"],
-    oath_key_dict["access_token"],
-    oath_key_dict["access_token_secret"]
-    )
-    return oath
-
-def tweet_search(search_word,max_id,oath_key_dict):
-    url = "https://api.twitter.com/1.1/search/tweets.json?"
-    params = {
-        "q": search_word,
-        "lang": "ja",
-        "result_type": "recent",
-        "count": "100",
-        "max_id": max_id - 1,
-        }
-    oath = create_oath_session(oath_key_dict)
-    responce = oath.get(url, params = params)
-    tweets = json.loads(responce.text)
-    return tweets
-
-def change_ja_time(time):
-    t_list = time.split(" ")
-    t_cut = " ".join(t_list[:4] + t_list[5:])
-    dt = datetime.datetime.strptime(t_cut, "%a %b %d %H:%M:%S %Y")
-    dt += datetime.timedelta(hours=9)
-    twit_at = dt.strftime("%Y-%m-%d %H:%M:%S")
-    return twit_at
