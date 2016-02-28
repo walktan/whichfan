5a2a3012e852a3d87deec1ed039da7f015395545
walktan <yb1.2st.50@gmail.com>
2016-02-28 21:42:07 +0900

    Fixes #1

--- wfan/cms/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class Wfan001Config(AppConfig):
-    name = 'cms'
--- wfan/cms/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import patterns, include, url
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-
-urlpatterns = patterns('',
-    url(r'^$', 'cms.views.index'),
-    url(r'^changeTerm/', 'cms.views.get_json'),
-)
-
-urlpatterns += staticfiles_urlpatterns()
