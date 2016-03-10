673d7434957803a3bdf49e5b04a76e6f1a0e0328
walktan <yb1.2st.50@gmail.com>
2016-03-10 23:10:54 +0900

    Fixes #12 conform to pep8

--- wfan/cms/urls.py
+++ wfan/cms/urls.py
@@ -2,8 +2,8 @@ from django.conf.urls import patterns, include, url
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = patterns('',
-    url(r'^$', 'cms.views.index'),
-    url(r'^changeTerm/', 'cms.views.get_json'),
-)
+                       url(r'^$', 'cms.views.index'),
+                       url(r'^changeTerm/', 'cms.views.get_json'),
+                       )
 
 urlpatterns += staticfiles_urlpatterns()
--- wfan/wfan/wsgi.py
+++ wfan/wfan/wsgi.py
@@ -1,13 +1,8 @@
-"""
-WSGI config for wfan project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
-"""
-
-import os,sys,site
+import os
+import sys
+import site
+from django.core.wsgi import get_wsgi_application
+from django.core.wsgi import get_wsgi_application
 
 # virtualenvのパッケージパス
 site.addsitedir("/home/ec2-user/env001/lib/python3.4/site-packages")
@@ -15,12 +10,10 @@ site.addsitedir("/home/ec2-user/env001/lib64/python3.4/site-packages")
 sys.path.append('/opt/python/current/app/wfan')
 sys.path.append('/opt/python/current/app/wfan/wfan')
 
-from django.core.wsgi import get_wsgi_application
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "wfan.settings")
 
 # virtualenvの実行コードのパス
-exec(compile(open('/home/ec2-user/env001/bin/activate_this.py').read(), '/home/ec2-user/env001/bin/activate_this.py', 'exec'))
+exec(compile(open('/home/ec2-user/env001/bin/activate_this.py').read(),
+             '/home/ec2-user/env001/bin/activate_this.py', 'exec'))
 
-from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
